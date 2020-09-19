a345bc4b9bf52af879f7d7c6e0cbc305f1e28ab4
daredevil1302 <markperanovic@gmail.com>
2020-09-19 11:42:38 +0200

    blabla commitam da mogu radit na thinkpadu

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -35,7 +35,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef;
 };
-
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
